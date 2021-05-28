518bbacf84661edd929825739212e8bb122fcfb2
Mike <mike@primarywavemedia.com>
2021-05-28 19:35:51 -0400

    api fix

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,20 @@
-module.exports = (req, res) => {
-  res.json('hello')
-}
+const app = require('express')()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.end('Hello! Go to item:')
+})
+
+app.get('/test', (req, res) => {
+  res.status(200).json('test - get').end()
+})
+
+app.post('/', (req, res) => {
+  res.status(200).json('test - post').end()
+})
+
+module.exports = app
