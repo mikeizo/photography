077a5a0e4526d6701020f0833c83ce7b5959f0d8
Mike <mike@primarywavemedia.com>
2021-05-29 08:54:07 -0400

    api test

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 const app = require('express')()
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json())
+
+app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.end('Hello! Go to item:')
 })
 
-app.get('/test', (req, res) => {
+app.get('/api/test', (req, res) => {
   res.status(200).json('test - get').end()
 })
 
-app.post('/', (req, res) => {
+app.post('/api', (req, res) => {
   res.status(200).json('test - post').end()
 })
 
