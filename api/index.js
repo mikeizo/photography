67e09e7cfa381342d9410b95c0e282422af37867
Mike <mike@primarywavemedia.com>
2021-05-30 11:22:23 -0400

    api fix

--- api/index.js
+++ api/index.js
@@ -9,6 +9,20 @@ import authenticateJWT from '../utils/authenticate'
 const app = express()
 app.use(bodyParser.json())
 
+app.get('/api', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.end('Hello! Go to item:')
+})
+
+app.get('/api/test', (req, res) => {
+  res.status(200).json('test - get').end()
+})
+
+app.post('/api/test', (req, res) => {
+  res.status(200).json('test - post').end()
+})
+
 // Get Settings
 app.get('/api/settings', async (req, res) => {
   const { db } = await connectToDatabase()
